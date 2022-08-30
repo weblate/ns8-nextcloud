3af35d21fdf85870cad385869841c34ad820e016
Andrea Leardini <andre8244@gmail.com>
2022-08-30 14:26:53 +0200

    config: disable the web based updater

--- nextcloud/ns.config.php
+++ nextcloud/ns.config.php
@@ -3,6 +3,9 @@
 // NethServer config
 $CONFIG = array(
    'updatechecker' => false,
+   'upgrade.disable-web' => true,
+   'updater.server.url' => 'https://localhost',
+   'has_internet_connection' => false,
    'check_for_working_wellknown_setup' => false,
    'logtype' => 'errorlog'
 );
