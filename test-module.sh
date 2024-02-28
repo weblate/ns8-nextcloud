932f57f680a0394e356092f7e4a74fc11b1857c8
Davide Principi <davide.principi@nethesis.it>
2024-02-28 17:45:53 +0100

    test. Update automated test script
    Downgrade rfbrowser to version 17

--- test-module.sh
+++ test-module.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2023 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+set -e
+
 LEADER_NODE=$1
 IMAGE_URL=$2
+shift 2
 SSH_KEYFILE=${SSH_KEYFILE:-$HOME/.ssh/id_rsa}
 
-ssh_key="$(cat $SSH_KEYFILE)"
+ssh_key="$(< $SSH_KEYFILE)"
 
+cleanup() {
+    set +e
+    podman cp rf-core-runner:/home/pwuser/outputs tests/
+    podman stop rf-core-runner
+    podman rm rf-core-runner
+}
+
+trap cleanup EXIT
 podman run -i \
+    --network=host \
     -v .:/home/pwuser/ns8-module:z \
-    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:18.2.0
+    --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
+    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:17.5.2 \
     bash -l -s <<EOF
-    set -e
-    echo "$ssh_key" > /home/pwuser/ns8-key
-    set -x
-    pip install -r /home/pwuser/ns8-module/tests/pythonreq.txt
-    mkdir ~/outputs
-    cd /home/pwuser/ns8-module
-    robot -v NODE_ADDR:${LEADER_NODE} \
-        -v IMAGE_URL:${IMAGE_URL} \
-        -v SSH_KEYFILE:/home/pwuser/ns8-key \
-	-d ~/outputs /home/pwuser/ns8-module/tests/
+set -e
+echo "$ssh_key" > /home/pwuser/ns8-key
+pip install -q -r /home/pwuser/ns8-module/tests/pythonreq.txt
+mkdir ~/outputs
+cd /home/pwuser/ns8-module
+exec robot -v NODE_ADDR:${LEADER_NODE} \
+    -v IMAGE_URL:${IMAGE_URL} \
+    -v SSH_KEYFILE:/home/pwuser/ns8-key \
+    --name openldap \
+    --skiponfailure unstable \
+    -d ~/outputs ${@} /home/pwuser/ns8-module/tests/
 EOF
-
-tests_res=$?
-
-podman cp rf-core-runner:/home/pwuser/outputs tests/
-podman stop rf-core-runner
-podman rm rf-core-runner
-
-exit ${tests_res}
