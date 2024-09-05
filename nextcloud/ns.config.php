2849ba122ff05f43943e4667581c65fe0186365b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-09-05 14:37:13 +0200

    feat: add maintenance window start and trusted proxies to Nextcloud config

--- nextcloud/ns.config.php
+++ nextcloud/ns.config.php
@@ -5,5 +5,7 @@ $CONFIG = array(
    'upgrade.disable-web' => true,
    'updatechecker' => false,
    'check_for_working_wellknown_setup' => false,
-   'log_type' => 'errorlog'
+   'log_type' => 'errorlog',
+   'maintenance_window_start' => 1,
+   'trusted_proxies' => ['127.0.0.1'],
 );
