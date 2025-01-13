da42d11135bed78e53fa5a8fd11432b0efb75b53
Davide Principi <davide.principi@nethesis.it>
2025-01-13 12:23:53 +0100

    chore: update RF 19.1.2

--- test-module.sh
+++ test-module.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2023 Nethesis S.r.l.
+# Copyright (C) 2024 Nethesis S.r.l.
 # SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+#
+# Hint: access the test logs on HTTP port 8000 with this command:
+#
+#     python -mhttp.server -d tests/outputs/ 8000 &
 #
 
 set -e
 
-LEADER_NODE=$1
-IMAGE_URL=$2
-shift 2
 SSH_KEYFILE=${SSH_KEYFILE:-$HOME/.ssh/id_rsa}
+
+LEADER_NODE="${1:?missing LEADER_NODE argument}"
+IMAGE_URL="${2:?missing IMAGE_URL argument}"
+shift 2
 
 ssh_key="$(< $SSH_KEYFILE)"
 
@@ -24,9 +31,9 @@ cleanup() {
 trap cleanup EXIT
 podman run -i \
     --network=host \
-    -v .:/home/pwuser/ns8-module:z \
-    --volume=site-packages:/home/pwuser/.local/lib/python3.8/site-packages:Z \
-    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:19.1.1 \
+    --volume=.:/home/pwuser/ns8-module:z \
+    --volume=site-packages:/home/pwuser/.local/lib/python3.12/site-packages:z \
+    --name rf-core-runner ghcr.io/marketsquare/robotframework-browser/rfbrowser-stable:19.1.2 \
     bash -l -s <<EOF
 set -e
 echo "$ssh_key" > /home/pwuser/ns8-key
@@ -36,7 +43,7 @@ cd /home/pwuser/ns8-module
 exec robot -v NODE_ADDR:${LEADER_NODE} \
     -v IMAGE_URL:${IMAGE_URL} \
     -v SSH_KEYFILE:/home/pwuser/ns8-key \
-    --name openldap \
+    --name "$(basename $PWD)" \
     --skiponfailure unstable \
     -d ~/outputs ${@} /home/pwuser/ns8-module/tests/
 EOF
